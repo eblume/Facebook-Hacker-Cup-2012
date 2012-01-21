c50ce949e2c73e98a174b0d1b237cfdea4913ec5
Erich Blume <blume.erich@gmail.com>
2012-01-21 12:16:11 -0800

    COPYING and billboard skeleton

---
+++ fbhc/billboard.py
@@ -0,0 +1,22 @@
+"""billboard.py - Fit fixed-width text on a billboard (Round 1)
+"""
+# Copyright 2012 Erich Blume <blume.erich@gmail.com>
+# ===========================
+#
+# This file is part of fbhc
+#
+# fbhc is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# fbhc is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with fbhc, in a file called COPYING.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+
